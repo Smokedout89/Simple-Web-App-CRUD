63df96054409c318967f54226eaa4925f91bdc39
Smokedout89 <smokedout8989@gmail.com>
2022-09-03 17:06:57 +0300

    Initial Upload

---
+++ WebAppCRUD/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAppCRUD.Models
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "This field is required!")]
+        [DisplayName("Full Name")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string FullName { get; set; }
+
+        [DisplayName("Employee Code")]
+        [Column(TypeName = "varchar(10)")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+        [DisplayName("Office Location")]
+        [Column(TypeName = "varchar(100)")]
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ WebAppCRUD/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAppCRUD.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
